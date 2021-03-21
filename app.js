d2691d811fb8aeb208c334fda32b508dd6978cff
P-Ramde <63366121+P-Ramde@users.noreply.github.com>
2021-03-21 19:17:04 +0100

    Final

--- app.js
+++ app.js
@@ -10,55 +10,69 @@ for (var x=0; x< boxes.children.length; x++)
 
 for (var i = 0; i < boxes.children.length; i++)
 {
-
   boxes.children[i].addEventListener("click", function()
   {
     this.className += " boxHuman"
-    var tall = listNumbers.indexOf(parseInt(this.id));
-    if(tall > -1)
+
+    var number = listNumbers.indexOf(parseInt(this.id));
+
+    if(number > -1)
     {
-      listNumbers.splice(tall,1);
+      listNumbers.splice(number,1);
     }
-    if(winningComboHuman()==true){
-      setTimeout(function(){
+
+    if(winningComboHuman()==true)
+    {
+      setTimeout(function()
+      {
         alert("Gratulerer du vant")
         return restart()
       },500)
     }
-    if (listNumbers.length==0){
-      setTimeout(function(){
+
+    if (listNumbers.length==0 && winningComboHuman() == false)
+    {
+      setTimeout(function()
+      {
         alert("Uavgjort")
         return restart()
       },500)
     }
+
     if (listNumbers.length > 0 && winningComboHuman() == false)
     {
-      var valgt = Math.floor(Math.random()*listNumbers.length)
-      var test = listNumbers[valgt];
-      document.getElementById(test).className += " boxComputer"
-      if(valgt > -1)
+      var random = Math.floor(Math.random()*listNumbers.length)
+      var chosen = listNumbers[random];
+
+      document.getElementById(chosen).className += " boxComputer"
+
+      if(random > -1)
       {
-        listNumbers.splice(valgt,1);
+        listNumbers.splice(random,1);
       }
     }
-    if(winningComboComputer()==true){
-      setTimeout(function(){
+
+    if(winningComboComputer()==true)
+    {
+      setTimeout(function()
+      {
         alert("Du tapte desverre")
         return restart()
       },500)
     }
   })
-  // Gjør at det blir en sirkel når musen beveger seg over.
+
   boxes.children[i].addEventListener("mouseover", function()
   {
     this.style.backgroundImage = "url('img/o.svg')";
   })
-  // Gjør at sirkelen forvinner når musen fjernes
+
   boxes.children[i].addEventListener("mouseout", function()
   {
     this.style.backgroundImage = "";
   })
 }
+
 function winningComboHuman(){
   if (
     (boxes.children[0].className == human && boxes.children[1].className == human && boxes.children[2].className == human) ||
@@ -75,6 +89,7 @@ function winningComboHuman(){
     return false;
   }
 }
+
 function winningComboComputer(){
   if (
     (boxes.children[0].className == computer && boxes.children[1].className == computer && boxes.children[2].className == computer) ||
@@ -91,6 +106,7 @@ function winningComboComputer(){
     return false;
   }
 }
+
 function restart(){
   listNumbers = [0,1,2,3,4,5,6,7,8]
   for (var y = 0; y < boxes.children.length; y++){
